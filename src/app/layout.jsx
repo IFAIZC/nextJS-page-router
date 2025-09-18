0a2e1214daa02ba246434636bb92a324ba24242b
Irfan Faiz Choo <165414147+IFAIZC@users.noreply.github.com>
2025-09-18 22:05:44 +0800

    add metadata

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,6 +2,11 @@ import "./globals.css";
 import Navbar from "../../components/navbar";
 import Footer from "../../components/footer";
 
+export const metadata = {
+  title: "NextJS - Practice",
+  description: "NextJS Fundamentals",
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
