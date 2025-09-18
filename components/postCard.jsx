abf1c252091af560e9e348c52dff6dcb47f7daaf
Irfan Faiz Choo <165414147+IFAIZC@users.noreply.github.com>
2025-09-18 22:59:35 +0800

    fixed sm responsive

--- components/postCard.jsx
+++ components/postCard.jsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export default function PostCard({img,title,description,alt}) {
   return (
-    <div className="card bg-base-300 w-50 md:w-50 lg:w-70 xl:w-90 shadow-sm mb-5 hover:scale-105 transition-all duration-300 hover:shadow-xl">
+    <div className="card bg-base-300 w-60 md:w-50 lg:w-70 xl:w-90 shadow-sm mb-5 hover:scale-105 transition-all duration-300 hover:shadow-xl">
       <div className="card-body h-40">
         <h2 className="card-title">{title}</h2>
         <p>{description}</p>
